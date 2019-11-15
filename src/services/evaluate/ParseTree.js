277868dc69ceee18c0235ba0269433552d0d8d8a
Mankaran Bedi <mankaran@cctech.co.in>
2019-11-15 15:49:00 +0530

    Added classes for data structures. WIP

---
+++ src/services/evaluate/ParseTree.js
@@ -0,0 +1,13 @@
+const Node = require('../evaluate/Node');
+
+class ParseTree {
+    constructor() {
+        this.root = null;
+    }
+    insert(data) {
+        if (this.root === null) {
+            let newNode = new Node(data);
+            this.root = newNode;
+        }
+    }
+}
